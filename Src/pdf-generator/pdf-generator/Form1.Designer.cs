973f7d1afc94446b3768815cbb808dfe8eb5030e
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-07-27 10:51:43 +0200

    Initial commit

---
+++ Src/pdf-generator/pdf-generator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace pdf_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 3);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(548, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(857, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 28);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Generate PDF";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Load from link";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Load from local";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1042, 859);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 859);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "pdf-generator";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Src/pdf-generator/pdf-generator/Form1.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WebView2 = Microsoft.Web.WebView2.WinForms.WebView2;
+
+namespace pdf_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static string filename = "";
+        public WebView2 webView21 = new WebView2();
+        private static int width;
+        private static int height;
+        private static int[] wd = { 2, 2, 2, 2, 2 };
+        private static int[] wu = { 2, 2, 2, 2, 2 };
+        private static void valchanged(int n, bool val)
+        {
+            if (val)
+            {
+                if (wd[n] <= 1)
+                {
+                    wd[n] = wd[n] + 1;
+                }
+                wu[n] = 0;
+            }
+            else
+            {
+                if (wu[n] <= 1)
+                {
+                    wu[n] = wu[n] + 1;
+                }
+                wd[n] = 0;
+            }
+        }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            width = CentimeterToPixel(21.0 + 3.8);
+            height = Screen.PrimaryScreen.Bounds.Height - 30;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - width / 2, 0);
+            this.Size = new Size(width, height);
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new Size(width, height - 21);
+            this.pictureBox1.Location = new Point(0, 21);
+            this.textBox1.AutoSize = false;
+            this.button1.Location = new Point(0, 0);
+            this.button1.Size = new Size(111, 21);
+            this.button2.Location = new Point(111, 0);
+            this.button2.Size = new Size(111, 21);
+            this.button3.Location = new Point(width - 111, 0);
+            this.button3.Size = new Size(94, 21);
+            this.textBox1.Location = new Point(222, 0);
+            this.textBox1.Size = new Size(width - 333, 21);
+            CoreWebView2EnvironmentOptions options = new CoreWebView2EnvironmentOptions("--disable-web-security", "--autoplay-policy=no-user-gesture-required");
+            CoreWebView2Environment environment = await CoreWebView2Environment.CreateAsync(null, null, options);
+            await webView21.EnsureCoreWebView2Async(environment);
+            webView21.CoreWebView2.SetVirtualHostNameToFolderMapping("appassets", "assets", CoreWebView2HostResourceAccessKind.DenyCors);
+            webView21.Source = new System.Uri("https://google.com");
+            webView21.NavigationCompleted += WebView21_NavigationCompleted;
+            webView21.MouseMove += WebView21_MouseMove;
+            webView21.Dock = DockStyle.Fill;
+            this.pictureBox1.Controls.Add(webView21);
+        }
+        private int CentimeterToPixel(double Centimeter)
+        {
+            double pixel = -1;
+            using (Graphics g = this.CreateGraphics())
+            {
+                pixel = Centimeter * g.DpiY / 2.54d;
+            }
+            return (int)pixel;
+        }
+        private void WebView21_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            textBox1.Text = webView21.Source.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            DialogResult result = MessageBox.Show(@"Click Ok to load a local file, or click Cancel to reload the local file previously load", "Open", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                OpenFileDialog op = new OpenFileDialog();
+                op.Filter = "All Files(*.*)|*.*";
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    filename = op.FileName;
+                    LoadPage();
+                }
+            }
+            else
+            {
+                LoadPage();
+            }
+        }
+        private void LoadPage()
+        {
+            webView21.CoreWebView2.Navigate(@"file:///" + filename);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri(textBox1.Text);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.ShowPrintUI();
+        }
+        private void settofrontButtons()
+        {
+            this.pictureBox1.SendToBack();
+        }
+        private void settofrontView()
+        {
+            this.pictureBox1.BringToFront();
+        }
+        private void WebView21_MouseMove(object sender, MouseEventArgs e)
+        {
+            valchanged(0, e.Y <= 21);
+            if (wd[0] == 1)
+            {
+                settofrontButtons();
+            }
+            if (wu[0] == 1)
+            {
+                settofrontView();
+            }
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            valchanged(1, e.Y <= 21);
+            if (wd[1] == 1)
+            {
+                settofrontButtons();
+            }
+            if (wu[1] == 1)
+            {
+                settofrontView();
+            }
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            valchanged(2, e.Y <= 21);
+            if (wd[2] == 1)
+            {
+                settofrontButtons();
+            }
+            if (wu[2] == 1)
+            {
+                settofrontView();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            webView21.Dispose();
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+    }
+}
